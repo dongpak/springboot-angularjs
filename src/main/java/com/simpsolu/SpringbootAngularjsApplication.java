4da04ee4c7c0b11c7099c0b2470fd4c1df198b9a
Dong Pak <dong.pak@ccur.com>
2017-09-12 18:30:57 -0400

    Initial checkin.

---
+++ src/main/java/com/simpsolu/SpringbootAngularjsApplication.java
@@ -0,0 +1,12 @@
+package com.simpsolu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngularjsApplication.class, args);
+	}
+}
